021012b19cf14c8b2a65f805d77e76328e3dea04
Mateoochoagaleano <158522384+Mateoochoagaleano@users.noreply.github.com>
2024-04-26 10:54:35 -0500

    actualizacion 26-04-2024 10:53
    menu principal
    menu rutas
    agregar rutas

---
+++ programa.py
@@ -0,0 +1,29 @@
+list_programacion = []
+list_rutas = []
+import os
+
+def fnt_limpiar():
+    os.system('cls')
+    print('NOMBRE DEL PROGRAMA: <<GEASTION DE RUTAS>>')
+    print('AUTOR: <<Mateo Ochoa>>')
+    print('AÑO: <<2024>>')
+    print('UNIVESIDAD CATOLICA LUISA AMIGO\n')
+
+
+
+sw2 = True
+
+def fnt_menu_rutas():
+    global sw2
+    while
+    fnt_limpiar()
+    opcion_r = input('<<MENU RUTAS>>\n1. AGREGAR\n2. consultar\n3. SALIR\n->')
+    if opcion_r == '1':
+        fnt_agregar_ruta()
+    if opcion_r == '3':
+        sw2 = False
+  
+sw = True
+while sw == True:
+    fnt_limpiar()
+    opcionStr = input('<<')
